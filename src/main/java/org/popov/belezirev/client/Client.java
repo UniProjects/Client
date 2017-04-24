3587b33c69bb86989ab621bca0b7038356c61366
megaboy2 <encho.belezirev@gmail.com>
2017-04-24 22:26:08 +0300

    Initial commit

---
+++ src/main/java/org/popov/belezirev/client/Client.java
@@ -0,0 +1,41 @@
+package org.popov.belezirev.client;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+	private String hostname;
+	private int port;
+	private String username;
+
+	public Client(String hostname, int port, String username) {
+		this.hostname = hostname;
+		this.port = port;
+		this.username = username;
+	}
+
+	public void start() {
+		try (Socket clientSocket = new Socket(hostname, port);
+				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+				BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				Scanner consoleInput = new Scanner(System.in)) {
+			String message = null;
+			while ((message = consoleInput.nextLine()) != null) {
+				writer.println(message);
+				writer.flush();
+				String received = reader.readLine();
+				if (received != null && !received.isEmpty()) {
+					System.out.println(received);
+				}
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
