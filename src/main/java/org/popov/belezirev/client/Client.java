9ffc2a7835bf7ea0a247e6f7fb01717b3d4e669a
Encho Belezirev <encho.belezirev@sap.com>
2017-04-30 15:00:48 +0300

    Add support for asynchronous reading/writing from/to server

--- src/main/java/org/popov/belezirev/client/Client.java
+++ src/main/java/org/popov/belezirev/client/Client.java
@@ -8,38 +8,53 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-	private String hostname;
-	private int port;
-	private String username;
+    private String hostname;
+    private int port;
+    private String username;
 
-	public Client(String hostname, int port, String username) {
-		this.hostname = hostname;
-		this.port = port;
-		this.username = username;
-	}
+    public Client(String hostname, int port, String username) {
+        this.hostname = hostname;
+        this.port = port;
+        this.username = username;
+    }
 
-	public void start() {
-		try (Socket clientSocket = new Socket(hostname, port);
-				PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
-				BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-				Scanner consoleInput = new Scanner(System.in)) {
-			while (true) {
-				String message = consoleInput.nextLine();
-				writer.println(message);
-				writer.flush();
-				String received = reader.readLine();
-				if (received != null && !received.isEmpty()) {
-					System.out.println(received);
-				}
-			}
+    public void start() {
+        try (Socket clientSocket = new Socket(hostname, port);
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            Scanner consoleInput = new Scanner(System.in)) {
+            Thread writerThread = new Thread(() -> {
+                    while (true) {
+                        String message = consoleInput.nextLine();
+                        writer.println(message);
+                        writer.flush();
+                    }
+                });
+            writerThread.start();
+            Thread readerThread = new Thread(() -> {
+                while (true) {
+                    try {
+                        String received = reader.readLine();
+                        if (received != null && !received.isEmpty()) {
+                            System.out.println(received);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            readerThread.start();
+            readerThread.join();
 
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e1) {
+            e1.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		new Client("localhost", 10513, "").start();
-	}
+    public static void main(String[] args) {
+        new Client("localhost", 10513, "").start();
+    }
 
 }
